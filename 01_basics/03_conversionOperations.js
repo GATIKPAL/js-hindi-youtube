1aec8a7eaac892b0ce0ae853bbff9debfb2f39bc
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-02-20 06:00:23 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+//let score = "234abc" 
+//let score = null
+//let score = undefined
+let score = true
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN ( Not a number) but type datastype is number
+//true => 1 and false => 0
+
+let isLoggedIn = 34
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => ture and 0 => false
+// "" => false
+// "GATIK" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
