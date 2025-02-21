f7e6af2582849b0257c5bccc18332233bb6712b8
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-02-21 07:42:26 +0000

    datatypes summary

---
+++ 01_basics/summary_of_datatypes.js
@@ -0,0 +1,30 @@
+// primitive(call by value)
+
+// 7types : string, Number, Boolean, Null, Undefined, symbol, BigInt
+
+
+
+//Non primitive(call by reference)
+
+//Array, Objects, Functions
+
+
+//JS is dynamically types language
+
+//Array example
+const heroes = ["dogo", "boka", "madara"];
+//Objects
+let myObj={
+    name : "Gatik",
+    age: 26,
+}
+//function
+const myFunction = function(){
+    console.log("HELLO WORLD");
+    
+}
+
+//****************************************************************************************************************************/
+
+//Stack (Primitive) , Heap (Non-primitive)
+
