2d80c6092c6f6f2b5f30ea6abe9866bbaed533c5
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-02-21 07:19:58 +0000

    compsrison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+//console.log("HELLO");
+//console.log(a > b);
+//console.log(a < b);
+//console.log(a == b);
+//console.log(a != b);
+
+console.log("1" > 2);//some time not giverequired results try to compare only same datatype
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// in comparison null is converted into zero thats why null>0 is false but null>=0 is true. Comparison and equality check 
+//works differently in java script
+
+// === is strict check it check not only values but also its datatypes
+
+
+
+
+
+
+
+
+
