85040d1b84c6c5b04336d091bbcb801ee3dfffe5
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-03-05 13:19:37 +0000

    done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+
+// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate = new Date("01-14-2025")
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
