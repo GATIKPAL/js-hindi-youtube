cb6ce478a72426d2017fd1d242f39d07e362dc73
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-03-11 11:57:05 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object() // give empty object only // diff yeh h ki yeh ek songleton object h
+const tinderUser = {} // and yeh non singleton object h
+
+tinderUser.id = "123abc "
+tinderUser.name = "sam"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3:"a", 4: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2)// {} is optional but its good to add
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
