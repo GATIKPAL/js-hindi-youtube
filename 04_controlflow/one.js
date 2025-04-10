4edc4f3592a6006a899bf43073a1ddf06d22976b
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-04-10 05:58:45 +0000

    if else

---
+++ 04_controlflow/one.js
@@ -0,0 +1,53 @@
+// if 
+ 
+const isUserLoggedIn = true
+// <,>,<=,>=,==,!=,=== yeh type bhi check karta h
+if(2=="2"){
+    //console.log("executed");
+    
+}
+
+if(2==="2"){
+    //console.log("executed");
+    
+}
+// !==
+if(3!=="2"){
+    //console.log("executed");
+    
+}
+
+
+
+//const temperatur = 41
+//if(temperatur=== 40){
+//    console.log("less than 50");
+//} else{
+//    console.log("greater than 50");
+//}
+//console.log('executed');
+
+
+
+
+
+//const score = 200
+//if (score > 100) {
+//    var power = 'fly' // so better to use let or const
+//    console.log(`user power: ${power}`);    
+//}
+//console.log(`user power: ${power}`);// var ki vajah se yeh bahar bhi execute ho para h kyuki var ka scope gloabl hota h
+
+const balance = 1000
+
+// if (balance> 500) console.log("test"),console.log("test2"); also its a bad practise to write code like this
+ // implicit scope , humne maan liya ki yaha scope h
+
+ if (balance < 500) {
+    console.log("less than ");
+    
+ } else if(balance> 750){
+    
+ }
+
+
