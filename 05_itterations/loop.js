d45da768a7aeb8afaf265cec56fe53da7c833d8f
GATIKPAL <153068067+GATIKPAL@users.noreply.github.com>
2025-04-14 07:17:27 +0000

    one

--- 05_itterations/loop.js
+++ 05_itterations/loop.js
@@ -3,10 +3,10 @@
 for (let i = 0; i < 10; i++) {
     const element = i;
     if (element==5){
-        console.log("5 is best");
+       // console.log("5 is best");
     }
 
-    console.log(element);
+   // console.log(element);
     
 }
 
@@ -25,6 +25,21 @@ for (let i = 1; i <= 10; i++) {
 let myArray = ["flash","Batman","Superman"]
 for (let i = 0; i < myArray.length; i++) {
     const element = myArray[i];
-    console.log(element);
+    //console.log(element);
     
 }
+
+
+// break and continue
+
+for (let index = 0; index <=20; index++) {
+    if (index==5){
+        console.log(`Detected 5`);
+        //break
+    }
+    console.log(`value of i ${index}`);
+    
+    
+}
+
+
